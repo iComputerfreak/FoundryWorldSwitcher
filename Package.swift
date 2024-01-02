7d4c94e9b1c61f46f9dd3021adff91819f0c162a
Jonas Frey <dev@jonasfrey.de>
2024-01-02 22:45:41 +0100

    Migrated Xcode project to SwiftPM project

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "FoundryWorldSwitcher",
+    platforms: [
+        .macOS(.v13)
+    ],
+    products: [
+        .executable(
+            name: "FoundryWorldSwitcher",
+            targets: ["FoundryWorldSwitcher"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/iComputerfreak/JFUtils", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://github.com/DiscordBM/DiscordBM", .upToNextMajor(from: "1.0.0")),
+        .package(url: "https://gitlab.com/mflint/HTML2Markdown", .upToNextMajor(from: "1.0.0")),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .executableTarget(
+        	name: "FoundryWorldSwitcher",
+        	dependencies: ["DiscordBM", "JFUtils", "HTML2Markdown"]
+        ),
+    ]
+)
