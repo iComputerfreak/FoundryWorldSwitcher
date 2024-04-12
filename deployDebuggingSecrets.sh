5f5136214a71df81b7627fc379e3ea18cee89a9e
Jonas Frey <dev@jonasfrey.de>
2024-04-12 16:53:05 +0200

    Add script to copy secrets to DerivedData

---
+++ deployDebuggingSecrets.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -e
+
+TARGET="$1"
+
+if [[ -z "$TARGET" ]]; then
+	echo "Please specify the target directory."
+	exit 1
+fi
+
+cp BOT_TOKEN "$1/BOT_TOKEN"
+cp PTERODACTYL_API_KEY "$1/PTERODACTYL_API_KEY"
+cp botConfig.json "$1/config/botConfig.json"
